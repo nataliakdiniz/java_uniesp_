d7eda7b7a46834816886b94ee49161c675729b0f
Natália Diniz <98323720+nataliakdiniz@users.noreply.github.com>
2022-10-02 17:40:05 -0300

    feat: classe produto

---
+++ venda_pos/Produto.java
@@ -0,0 +1,55 @@
+public class Produto {
+    private Integer codigo;
+    private String descricao;
+    private Double valorVenda;
+    private Double valorCusto;
+    private Boolean promocao;
+
+    Produto(Integer codigo, String descricao, Double valorVenda, Double valorCusto, Boolean promocao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.valorVenda = valorVenda;
+        this.valorCusto = valorCusto;
+        this.promocao = promocao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(Double valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public Double getValorCusto() {
+        return valorCusto;
+    }
+
+    public void setValorCusto(Double valorCusto) {
+        this.valorCusto = valorCusto;
+    }
+
+    public Boolean getPromocao() {
+        return promocao;
+    }
+
+    public void setPromocao(Boolean promocao) {
+        this.promocao = promocao;
+    }
+}
